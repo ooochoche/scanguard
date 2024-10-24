5eba813a8146df4c37b2ae2357b991b9f67b59d0
Ikem Peter <ikempeter2020@gmail.com>
2024-10-24 12:11:31 +0100

    improved getEvnPath

--- backend/src/common/utils/getEnvPath.ts
+++ backend/src/common/utils/getEnvPath.ts
@@ -2,5 +2,5 @@ export function getEnvPath() {
   //? we might implement some complex env file getting logic here
   //? for now we just return the sample env file
   
-  return ".env.sample";
+  return `${process.cwd()}/.env.sample`;
 }
