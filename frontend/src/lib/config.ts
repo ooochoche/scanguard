e14fdf754a226160c2666140f8a07fec4b5fab20
EjembiEmmanuel <ejembioche50@gmail.com>
2024-10-27 12:58:56 +0100

    chore: run prettier fix

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,33 +1,35 @@
-import TransgateConnect from "@zkpass/transgate-js-sdk";
+import TransgateConnect from '@zkpass/transgate-js-sdk';
 
-export const CONTRACT_ADDR = "0x77d35c666955199a1ebf7424089f29f48a635b349a95614a763a63d62f61779";
+export const CONTRACT_ADDR =
+  '0x77d35c666955199a1ebf7424089f29f48a635b349a95614a763a63d62f61779';
 
 export const formatAddress = (addr: string) => {
-	return addr.replace(/^0x/, "0x0");
+  return addr.replace(/^0x/, '0x0');
 };
 
 export const formatIpfsHash = (hash: string) => {
-	return hash.replace(/,/g, "");
+  return hash.replace(/,/g, '');
 };
 
 export const zkPassVerify = async () => {
-	try {
-		const appid = "8fb9d43c-2f24-424e-a98d-7ba34a5532f5";
+  try {
+    const appid = '8fb9d43c-2f24-424e-a98d-7ba34a5532f5';
 
-		const connector = new TransgateConnect(appid);
+    const connector = new TransgateConnect(appid);
 
-		const isAvailable = await connector.isTransgateAvailable();
+    const isAvailable = await connector.isTransgateAvailable();
 
-		if (isAvailable) {
-			const schemaId = "516a720e-29a4-4307-ae7b-5aec286e446e";
+    if (isAvailable) {
+      const schemaId = '516a720e-29a4-4307-ae7b-5aec286e446e';
 
-			const res = await connector.launch(schemaId);
-		} else {
-			console.log("Please install TransGate");
-		}
-	} catch (error) {
-		console.log("transgate error", error);
-	}
+      const res = await connector.launch(schemaId);
+    } else {
+      console.log('Please install TransGate');
+    }
+  } catch (error) {
+    console.log('transgate error', error);
+  }
 };
 
-export const formatDate = (date: string) => date.split('-').reverse().join('. ');
+export const formatDate = (date: string) =>
+  date.split('-').reverse().join('. ');
