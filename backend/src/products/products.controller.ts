ba73b857ce02976d53239a0142e4fa0d60c25d68
Kefas Kingsley <kefasiceking@gmail.com>
2024-10-29 10:01:52 +0100

    feat: migrate product submission logic from expressjs to nestjs, created DTO for product data, and upload products to IPFS

--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post, HttpException, HttpStatus } from '@nestjs/common';
+import { ProductDto } from './product.dto';
+import { ProductsService } from './products.service';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+    constructor(private readonly productService: ProductsService) {}
+
+    @Post('submit')
+    async submitProduct(@Body() productDto: ProductDto) {
+        console.log('Received DTO:', productDto);
+        try {
+            const ipfsHash = await this.productService.submitProduct(productDto);
+            console.log('Service Result:', ipfsHash);
+            return { ipfs_hash: ipfsHash };
+        } catch (error) {
+            console.error('Error:', error.message);
+
+            throw new HttpException(
+                { error: error.message },
+                HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+        }
+    }
+}
--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -1,4 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import { ProductDto } from './product.dto';
+import { generateProductId } from '../utils';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    private readonly PINATA_JWT = process.env.PINATA_JWT || '';
+
+    async pinToIPFS(product: any): Promise<any> {
+        const url = 'https://api.pinata.cloud/pinning/pinFileToIPFS';
+
+        const blob = new Blob([JSON.stringify(product, null, 2)], {
+        type: 'application/json',
+        });
+
+        const file = new File([blob], `${product.product_id}.txt`);
+        const data = new FormData();
+        data.append('file', file);
+
+        const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${this.PINATA_JWT}`,
+        },
+        body: data,
+        });
+
+        console.log("Response: ", await response);
+
+        if (!response.ok) {
+            const errorBody = await response.json();
+            console.error("Error Response:", errorBody);
+            throw new Error(`Failed to upload to IPFS: ${errorBody.message || response.statusText}`);
+        }
+
+        return await response.json();
+    }
+
+    async submitProduct(createProductDto: ProductDto): Promise<string> {
+        const product_id = generateProductId(10);
+        const productData = { product_id, ...createProductDto };
+    
+        try {
+            const pin = await this.pinToIPFS(productData);
+            return pin.IpfsHash;
+        } catch (error) {
+            throw new Error('Error uploading to IPFS');
+        }
+    }
+}
